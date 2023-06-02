01b01729b16cfe8c259dba01612c68d0de2b956e
Harshith Paladi <87645227+HarshithPaladi@users.noreply.github.com>
2023-06-02 16:17:59 +0530

    [Update] Nav Buttons

--- src/components/RicknMortyComponent/CharactersComponent.jsx
+++ src/components/RicknMortyComponent/CharactersComponent.jsx
@@ -1,29 +1,48 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import { useQuery } from "react-query";
 import CharacterComponent from "./CharacterComponent";
 
 function CharactersComponent() {
-    const getCharacters = async () => {
-        const response = await fetch("https://rickandmortyapi.com/api/character");
-        return response.json();
-    };
+	const [page, setPage] = useState(1);
 
-    const { data, status } = useQuery('characters', getCharacters);
-    console.log(status)
-	if (status === "loading") {
-		return <div>Loading....</div>;
+	const getCharacters = async ({ queryKey }) => {
+		const response = await fetch(
+			`https://rickandmortyapi.com/api/character/?page=${queryKey[1]}`
+		);
+		return response.json();
+	};
+
+	const { data, status, isPreviousData, isLoading, isError } = useQuery(
+		["characters", page],
+		getCharacters,
+		{ keepPreviousData: true }
+	);
+
+	if (isLoading) {
+		return <div>Loading...</div>;
 	}
-	if (status === "error") {
-		return <div>Error,,</div>;
+	if (isError) {
+		return <div>Error!!</div>;
 	}
 	return (
 		<Fragment>
 			<div>
 				{data.results.map((character) => (
 					<div>
-                        <CharacterComponent character={character} />
+						<CharacterComponent character={character} />
 					</div>
 				))}
+				<div>
+					<button disabled={page === 1} onClick={() => setPage(page - 1)}>
+						Previous Page
+					</button>
+					<button
+						disabled={isPreviousData && !data.info.next}
+						onClick={() => setPage(page + 1)}
+					>
+						Next Page
+					</button>
+				</div>
 			</div>
 		</Fragment>
 	);
